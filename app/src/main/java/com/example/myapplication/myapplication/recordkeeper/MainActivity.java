178cd4f86a0f062505e8c22120d26b500c36f495
Sepehr Naseri <naseris@cardiff.ac.uk>
2019-03-17 23:15:14 +0000

    Created a fully operational nights out checkbox and storing user checkbox inputs

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
@@ -12,24 +12,33 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox vehcileUse;
+    private CheckBox nightout;
 
-    public void addListenerOnCheckBox() {
-        vehcileUse = ((CheckBox)findViewById(R.id.vehicleUse));
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vehcileUse = ((CheckBox)findViewById(R.id.vehicleUse));
+        nightout = ((CheckBox)findViewById(R.id.nightout));
         AppCompatButton saveButton = findViewById(R.id.Save_Button);
 
+        
+
+        //Save button Listner
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override  //setting what happens when clicked below
             public void onClick(View v) {
+                StringBuffer result = new StringBuffer();
+                result.append("Vehicle Use: ").append(vehcileUse.isChecked());
+                result.append("Staying out at nights: ").append(nightout.isChecked());
 
                 Toast.makeText(  //toast pop up message creation
                         getApplicationContext(),  // came as this good if this cant b used
-                        String.format ("Your shift log has been saved"),
+//                        String.format ("Your shift log has been saved"),
+                        //Testing the checkboxes saved values
+                        result.toString(),
                         Toast.LENGTH_SHORT
                 ).show();
             }
