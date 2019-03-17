738ac23231f3cedcda5c98f5b3e735fa4f2c84de
Sepehr Naseri <naseris@cardiff.ac.uk>
2019-03-17 22:26:48 +0000

    Created a checkbox for a vehicle use

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.myapplication.myapplication.recordkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox vehcileUse;
+
+    public void addListenerOnCheckBox() {
+        vehcileUse = ((CheckBox)findViewById(R.id.vehicleUse));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
