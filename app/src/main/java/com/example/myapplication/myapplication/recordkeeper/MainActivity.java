f2074260e16e0a01e4b4ceba881ef12528bee819
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-03-17 19:21:18 +0000

    linking navigation tool bar to main activity

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
@@ -34,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
 
 }
