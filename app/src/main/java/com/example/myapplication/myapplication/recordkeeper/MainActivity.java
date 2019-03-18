40521eb9f71bed2b7ca766c8a869ff887d8646be
Sepehr Naseri <naseris@cardiff.ac.uk>
2019-03-18 00:27:24 +0000

    Clicking on pick start date opens up a calendar based date selector

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/MainActivity.java
@@ -41,7 +41,32 @@ public class MainActivity extends AppCompatActivity {
 
         //Date Listener
 
+        mDisplayStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Black,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+                dialog.show();
+            }
+
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "onStartDateSet: dd/mm/yyyy: " + dayOfMonth + "/" + month + "/" + year);
+
+            }
+        };
 
         //Checkbox click listeners
         vehcileUse.setOnClickListener(new View.OnClickListener() {
