54505af88d506651bb67d415163abc79898bbea2
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-03-18 14:22:06 +0000

    Removing uneeded code

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/navigation.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/navigation.java
@@ -1,13 +1,6 @@
 package com.example.myapplication.myapplication.recordkeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
