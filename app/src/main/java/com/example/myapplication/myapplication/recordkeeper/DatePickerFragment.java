da2259c8fbc47b53f29a9322d6bf23f888e96330
Tzavier Randle-Jolliffe <randle-jolliffet@cardiff.ac.uk>
2019-03-21 15:56:39 +0000

    Created Date Picker Fragment

---
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/DatePickerFragment.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.myapplication.recordkeeper;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(
+                getActivity(),
+                android.R.style.Theme_Black,
+                (DatePickerDialog.OnDateSetListener) getActivity(),
+                year,month,day);
+    }
+}
