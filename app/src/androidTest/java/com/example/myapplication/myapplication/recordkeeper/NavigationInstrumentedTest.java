7b2a29eff52f32df40d228a0e5820afadc8e0e5d
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-04-01 14:30:31 +0100

    setting up test class

---
+++ app/src/androidTest/java/com/example/myapplication/myapplication/recordkeeper/NavigationInstrumentedTest.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.myapplication.recordkeeper;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class NavigationInstrumentedTest {
+
+  /*  @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivtyTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void secondActivityLaunchTest() {
+
+        Intents.init();
+        Espresso.onView(
+                ViewMatchers.withId(R.id.)
+        ).perform(
+                ViewActions.click()
+        );
+
+        Intents.intended(
+                IntentMatchers.hasComponent(SecondActivity.class.getName())
+        );
+        Intents.release();
+
+    }*/
+}
