6b47fbf2ae36a49a0d1929344b2ed1423b1e603e
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-04-05 12:47:29 +0100

    created validation on agency phone numbers

--- app/src/main/java/com/example/myapplication/myapplication/recordkeeper/NewAgencyFragment.java
+++ app/src/main/java/com/example/myapplication/myapplication/recordkeeper/NewAgencyFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,9 +39,31 @@ public class NewAgencyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_new_agency, container, false);
-        return view;
+        final View view = inflater.inflate(R.layout.activity_new_agency, container, false);
+
+        view.findViewById(R.id.Agency_Save_Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText phoneNumber = ((EditText) view.findViewById(R.id.phoneNumberInput));
+                if(phoneNumber.getText().length() != 11) {
+                    Toast.makeText(view.getContext(), "Phone number must be 11 digits", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!Pattern.matches("\\d+", phoneNumber.getText())) {
+                    Toast.makeText(view.getContext(), "Phone number must be all numbers", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+
+       return view;
+
+
     }
+
+
+
 
 
 
