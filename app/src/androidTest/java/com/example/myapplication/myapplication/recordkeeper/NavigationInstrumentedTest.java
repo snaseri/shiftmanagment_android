5c554ebee47bd291f0903b874edcfa03cf171637
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-04-07 16:13:33 +0100

    example navigation test

--- app/src/androidTest/java/com/example/myapplication/myapplication/recordkeeper/NavigationInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/myapplication/recordkeeper/NavigationInstrumentedTest.java
@@ -1,14 +1,6 @@
 package com.example.myapplication.myapplication.recordkeeper;
 
-import android.content.Context;
-import android.provider.ContactsContract;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.intent.matcher.IntentMatchers;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
+
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -25,23 +17,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class NavigationInstrumentedTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mainActivityActivtyTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
-    @Test
-    public void secondActivityLaunchTest() {
-
-        Intents.init();
-        Espresso.onView(
-                ViewMatchers.withId(R.id.past_logs)
-        ).perform(
-                ViewActions.click()
-        );
-
-        Intents.intended(
-                IntentMatchers.hasComponent(PastShiftLogsFragment.class.getName())
-        );
-        Intents.release();
 
     }
-}
+
