b42c7201ccbe65326f1291ffd2ace25791ecf7f9
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-04-08 16:52:51 +0100

    Creating test classes

---
+++ app/src/test/java/com/example/myapplication/myapplication/recordkeeper/PhoneNoHasNosTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.myapplication.recordkeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class PhoneNoHasNosTest {
+    @Test
+    public void phoneNoHasNos() {
+
+    }
+}
