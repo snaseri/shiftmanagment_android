63bcedd6f4ba2a018569bbecd0faa8bd0145da7f
Laura Camilleri <Camilleril2@cardiff.ac.uk>
2019-04-08 17:52:25 +0100

    Creationing option menu navigation test

--- app/src/androidTest/java/com/example/myapplication/myapplication/recordkeeper/navigationTest.java
+++ app/src/androidTest/java/com/example/myapplication/myapplication/recordkeeper/navigationTest.java
@@ -2,6 +2,7 @@ package com.example.myapplication.myapplication.recordkeeper;
 
 
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -20,6 +21,8 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -34,35 +37,16 @@ public class navigationTest {
 
     @Test
     public void navigationTest() {
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        // open options menu of main activity
+        openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity());
 
+        // after opening options menu
+        // find menu item with title past logs
         ViewInteraction appCompatTextView = onView(
                 allOf(withId(R.id.title), withText("Past Logs"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.content),
-                                        0),
-                                0),
                         isDisplayed()));
         appCompatTextView.perform(click());
-    }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        onView(withId(R.id.list)).check(matches(isDisplayed()));
     }
 }
